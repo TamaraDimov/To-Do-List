cfb59a8841a3b190873ea9fbdd1dc3851279168e
TamaraDimov <tamemojsoska@gmail.com>
2023-01-18 12:42:09 +0100

    add .sort

--- src/index.js
+++ src/index.js
@@ -9,25 +9,24 @@ const tasks = [
   {
     description: 'Wash the dishes',
     completed: true,
-    index: 2,
+    index: 3,
   },
   {
     description: 'Buy groceries',
     completed: false,
-    index: 3,
+    index: 2,
   },
 ];
 
 const taskList = () => {
   let content = '';
-  tasks.forEach((task) => {
-    content += `
-    <ul  id="task-list">
-        <li class="task-list" id="toDo"><input type="checkbox" id="checkbox"  value="task">${task.description}</li>
-    </ul>
-
-    `;
-  });
+  tasks
+    .sort((a, b) => a.index - b.index)
+    .forEach((task) => {
+      content += `
+            <li class="task-list" id="toDo"><input type="checkbox" id="${task.description}"  value="task">${task.description}</li>
+        `;
+    });
   return content;
 };
 
