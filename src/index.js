f89700a69eb670ac3526121f34e9fac4ae417cb3
TamaraDimov <tamemojsoska@gmail.com>
2023-01-19 12:16:49 +0100

    add new feature and update readme

--- src/index.js
+++ src/index.js
@@ -1,38 +1,130 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Take out the trash',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Wash the dishes',
-    completed: true,
-    index: 3,
-  },
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 2,
-  },
-];
-
-const taskList = () => {
-  let content = '';
-  tasks
-    .sort((a, b) => a.index - b.index)
-    .forEach((task) => {
-      content += `
-            <li class="task-list" id="toDo"><input type="checkbox" id="${task.description}"  value="task">${task.description}</li>
-        `;
-    });
-  return content;
+const updateLocalStorage = (toDoListArray) => {
+  localStorage.setItem('toDoList', JSON.stringify(toDoListArray));
 };
 
-const rendering = () => document.getElementById('to-dos');
+const getLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem('toDoList'));
+  let toDoListArray;
+  if (localStorageData) {
+    toDoListArray = localStorageData;
+  } else {
+    toDoListArray = [];
+  }
+  return toDoListArray;
+};
 
-window.addEventListener('load', () => {
-  const content = taskList();
-  rendering().innerHTML = content;
+const toDoTasks = getLocalStorage();
+const toDoList = document.querySelector('.toDoContainer');
+
+const addTask = (toDoListArray, task) => {
+  toDoListArray.push({ task, id: toDoListArray.length + 1 });
+};
+
+const renderToDoList = (toDoListArray) => {
+  toDoList.innerHTML = '';
+
+  toDoListArray = getLocalStorage();
+
+  toDoListArray.forEach((toDo) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.classList.add('toDoContainer-li');
+
+    const toDoCheckbox = document.createElement('input');
+    toDoCheckbox.classList.add('toDoContainer-li-checkbox');
+    toDoCheckbox.type = 'checkbox';
+    toDoItem.appendChild(toDoCheckbox);
+
+    const toDoText = document.createElement('input');
+    toDoText.classList.add('toDoContainer-li-text');
+    toDoText.value = toDo.task;
+    toDoItem.appendChild(toDoText);
+
+    if (toDo.completed) {
+      toDoText.classList.add('completed');
+    }
+
+    const crossIcon = document.createElement('span');
+    crossIcon.classList.add('trash-can');
+    crossIcon.innerHTML = '🗑️';
+    toDoItem.appendChild(crossIcon);
+
+    toDoList.appendChild(toDoItem);
+  });
+};
+
+const editTask = (e, toDoListArray) => {
+  const clickedTask = e.target.closest('.toDoContainer-li-text');
+  clickedTask.disabled = false;
+  clickedTask.focus();
+  const taskText = clickedTask.value;
+};
+
+const deleteTask = (e, toDoListArray) => {
+  const clickedCross = e.target.closest('.trash-can');
+  const clickedTask = clickedCross.previousElementSibling;
+  const taskIndex = toDoListArray.findIndex(
+    (task) => task.task === clickedTask.value
+  );
+  toDoListArray.splice(taskIndex, 1);
+  toDoListArray.forEach((task, index) => {
+    task.id = index + 1;
+  });
+  updateLocalStorage(toDoListArray);
+  renderToDoList(toDoListArray);
+};
+
+const markTask = (e, toDoListArray) => {
+  const clickedCheckbox = e.target.closest('.toDoContainer-li-checkbox');
+  const clickedTask = clickedCheckbox.nextElementSibling;
+  const taskIndex = toDoListArray.findIndex(
+    (task) => task.task === clickedTask.value
+  );
+  toDoListArray[taskIndex].completed = !toDoListArray[taskIndex].completed;
+  updateLocalStorage(toDoListArray);
+  renderToDoList(toDoListArray);
+};
+
+const input = document.querySelector('.inputField');
+const todoList = document.querySelector('.toDoContainer');
+const clearCompletedBtn = document.querySelector('.clearbtn');
+const addTaskBtn = document.querySelector('.clearbtn');
+
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && input.value !== '') {
+    addTask(toDoTasks, input.value);
+    input.value = '';
+    updateLocalStorage(toDoTasks);
+    renderToDoList(toDoTasks);
+  }
 });
+
+addTaskBtn.addEventListener('click', () => {
+  if (input.value !== '') {
+    addTask(toDoTasks, input.value);
+    input.value = '';
+    updateLocalStorage(toDoTasks);
+    renderToDoList(toDoTasks);
+  }
+});
+
+todoList.addEventListener('click', (e) => {
+  if (e.target.closest('.toDoContainer-li-checkbox')) {
+    markTask(e, toDoTasks);
+  }
+});
+
+todoList.addEventListener('dblclick', (e) => {
+  if (e.target.closest('.toDoContainer-li-text')) {
+    editTask(e, toDoTasks);
+  }
+});
+
+todoList.addEventListener('click', (e) => {
+  if (e.target.closest('.trash-can')) {
+    deleteTask(e, toDoTasks);
+  }
+});
+
+renderToDoList(toDoTasks);
