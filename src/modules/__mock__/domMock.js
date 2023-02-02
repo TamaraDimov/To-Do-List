44ff5a68a8c1b4d06b766a2adeb857a4d87d873a
BB-Simon <simon.chowdery@gmail.com>
2023-02-02 13:08:03 +0200

    Add test to add and remove item to/from dom list

--- src/modules/__mock__/domMock.js
+++ src/modules/__mock__/domMock.js
@@ -1,33 +1,10 @@
-document.body.innerHTML = `
-<section class="toDo_list">
-  <div class="title">
-    <h1 class="toDo_title">Today's To Do</h1>
-  </div>
-  <div class="container">
-    <input
-      type="text"
-      name="task"
-      id="task"
-      class="inputField"
-      placeholder="Add new task"
-    />
-    <button class="insert-btn">Insert</button>
-  </div>
-  <ul class="toDoContainer"></ul>
-  <button class="clearbtn">Clear all completed</button>
-</section>
-})`;
+const renderToDoListMock = (toDoListArray, ul) => {
+  ul.innerHTML = '';
+  toDoListArray.forEach((toDo) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.innerHTML = toDo.description;
+    ul.appendChild(toDoItem);
+  });
+};
 
-const todoList = document.querySelector('.toDoContainer');
-const tasks = [
-  {
-    description: 'task1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task2',
-    completed: false,
-    index: 2,
-  },
-];
+export default renderToDoListMock;
--- src/modules/lists.test.js
+++ src/modules/lists.test.js
@@ -8,6 +8,29 @@ import {
   deleteTaskMock,
 } from './__mock__/listMock.js';
 import localStorage from './__mock__/localStorageMock.js';
+import renderToDoListMock from './__mock__/domMock.js';
+
+document.body.innerHTML = `
+<section class="toDo_list">
+  <div class="title">
+    <h1 class="toDo_title">Today's To Do</h1>
+  </div>
+  <div class="container">
+    <input
+      type="text"
+      name="task"
+      id="task"
+      class="inputField"
+      placeholder="Add new task"
+    />
+    <button class="insert-btn">Insert</button>
+  </div>
+  <ul class="toDoContainer"></ul>
+  <button class="clearbtn">Clear all completed</button>
+</section>
+})`;
+
+const ul = document.querySelector('.toDoContainer');
 
 describe('Test', () => {
   test('Add a new item to todo list', () => {
@@ -16,9 +39,21 @@ describe('Test', () => {
     expect(localStorage.getItem('tasks')).toHaveLength(1);
   });
 
+  test('Add a new item to DOM todo list', () => {
+    renderToDoListMock(toDoListArray, ul);
+    const lis = Array.from(ul.children);
+    expect(lis).toHaveLength(1);
+  });
+
   test('Remove only one item', () => {
     deleteTaskMock(0);
     expect(toDoListArray).toHaveLength(0);
     expect(localStorage.getItem('tasks')).toHaveLength(0);
+  });
+
+  test('Remove one item from DOM todo list', () => {
+    renderToDoListMock(toDoListArray, ul);
+    const lis = Array.from(ul.children);
+    expect(lis).toHaveLength(0);
   });
 });
